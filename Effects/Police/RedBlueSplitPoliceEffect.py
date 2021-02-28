c04ee916480b09a30bfafc3ce18ec27939185be3
Luke Anderson <stuntguy3000@gmail.com>
2021-02-28 13:36:57 +1030

    Fix Copyright

--- Effects/Police/RedBlueSplitPoliceEffect.py
+++ Effects/Police/RedBlueSplitPoliceEffect.py
@@ -1,6 +1,6 @@
 255, 0  # The MIT License
 #
-#  Copyright (c) 2020 Luke Anderson (stuntguy3000)
+#  Copyright (c) 2021 Luke Anderson (stuntguy3000)
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
--- Effects/Util/SegmentUtil.py
+++ Effects/Util/SegmentUtil.py
@@ -1,6 +1,6 @@
 #  The MIT License
 #
-#  Copyright (c) 2020 Luke Anderson (stuntguy3000)
+#  Copyright (c) 2021 Luke Anderson (stuntguy3000)
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 #
-#  Copyright (c) 2020 Luke Anderson (stuntguy3000)
+#  Copyright (c) 2021 Luke Anderson (stuntguy3000)
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
